ca0dee6b57479549f87bdf97f31d46bc59c40a73
quanix <quanix@163.com>
2014-11-20 21:51:06 +0800

    增加存档, 标签 , 搜索 功能

--- models/post.js
+++ models/post.js
@@ -2,9 +2,10 @@ var mongodb = require('./db');
 var markdown = require('markdown').markdown;
 
 /** 文章实体 **/
-function Post(name,title,post) {
+function Post(name,title,tags,post) {
     this.name = name;
     this.title = title;
+    this.tags = tags;
     this.post = post;
 }
 
@@ -27,8 +28,10 @@ Post.prototype.save = function(callback) {
         name : this.name,
         time : time,
         title: this.title,
+        tags: this.tags,
         post : this.post,
-        comments : []
+        comments : [],
+        pv:0
     }
 
     //打开数据库
@@ -169,19 +172,32 @@ Post.getOne = function(name,day,title,callback) {
                 "time.day":day,
                 "title":title
             },function(err,doc) {
-                mongodb.close();
                 if(err) {
+                    mongodb.close();
                     return callback(err);
                 }
 
                 if(doc) {
+                    //每访问 1 次，pv 值增加 1
+                    collection.update({
+                        "name":name,
+                        "time.day":day,
+                        "title":title
+                    },{
+                        $inc :{"pv":1}
+                    },function(err) {
+                        mongodb.close();
+                        if(err) {
+                            return callback(err);
+                        }
+                    });
+
                     doc.post = markdown.toHTML(doc.post);
                     doc.comments.forEach(function(comment) {
                         comment.content = markdown.toHTML(comment.content);
                     });
+                    callback(null,doc);//返回查询的一篇文章
                 }
-
-                callback(null,doc);//返回查询的一篇文章
             });
         });
     });
@@ -287,24 +303,129 @@ Post.remove = function(name,day,title,callback) {
 //返回所有文章存档信息
 Post.getArchive = function(callback) {
 
+    mongodb.open(function(err,db) {
+        if(err) {
+            return callback(err);
+        }
+
+        //读取posts集合
+        db.collection('posts',function(err,collection) {
+            if(err) {
+                mongodb.close();
+                return callback(err);
+            }
+
+            collection.find({},{
+                "name":1,
+                "time":1,
+                "title":1
+            }).sort({
+                time:-1
+            }).toArray(function(err,docs) {
+                mongodb.close();
+                if(err) {
+                    return callback(err);
+                }
+                callback(null,docs);
+            });
+        });
+    });
 };
 
 
 //返回所有标签
-Post.getTags = function (callback) {
-
+Post.getTags = function(callback) {
+    //打开数据库
+    mongodb.open(function (err, db) {
+        if (err) {
+            return callback(err);
+        }
+        //读取 posts 集合
+        db.collection('posts', function (err, collection) {
+            if (err) {
+                mongodb.close();
+                return callback(err);
+            }
+            //distinct 用来找出给定键的所有不同值
+            collection.distinct("tags", function (err, docs) {
+                mongodb.close();
+                if (err) {
+                    return callback(err);
+                }
+                callback(null, docs);
+            });
+        });
+    });
 };
 
-
-//返回含有特地标签的所有文章
-Post.getTag = function(callback) {
-
+//返回含有特定标签的所有文章
+Post.getTag = function(tag, callback) {
+    mongodb.open(function (err, db) {
+        if (err) {
+            return callback(err);
+        }
+        db.collection('posts', function (err, collection) {
+            if (err) {
+                mongodb.close();
+                return callback(err);
+            }
+            //查询所有 tags 数组内包含 tag 的文档
+            //并返回只含有 name、time、title 组成的数组
+            collection.find({
+                "tags": tag
+            }, {
+                "name": 1,
+                "time": 1,
+                "title": 1
+            }).sort({
+                time: -1
+            }).toArray(function (err, docs) {
+                mongodb.close();
+                if (err) {
+                    return callback(err);
+                }
+                callback(null, docs);
+            });
+        });
+    });
 };
 
 
 //返回通过关键字查询的所有文章
 Post.search = function(keyword,callback) {
 
+    mongodb.open(function(err,db) {
+        if(err) {
+            return callback(err);
+        }
+
+        db.collection('posts',function(err,collection) {
+            if(err) {
+                mongodb.close();
+                return callback(err);
+            }
+
+            var pattern = new RegExp("^.*" + keyword + ".*$", "i");
+            collection.find({ $or :[
+                {"title":pattern},
+                {"post":pattern}
+            ]
+            },{
+                "name": 1,
+                "time": 1,
+                "title": 1,
+                "post":1
+            }).sort({
+                time:-1
+            }).toArray(function(err,docs) {
+                mongodb.close();
+                if(err) {
+                    return callback(err);
+                }
+                callback(null,docs);
+            });
+        });
+    });
 };
 
 
--- routes/index.js
+++ routes/index.js
@@ -157,7 +157,10 @@ module.exports = function(app) {
   app.post('/post',checkLogin);
   app.post('/post',function(req,res) {
     var currentUser = req.session.user;
-    var post = new Post(currentUser.name,req.body.title,req.body.post);
+
+    var tags = [req.body.tag1, req.body.tag2, req.body.tag3];
+
+    var post = new Post(currentUser.name,req.body.title,tags,req.body.post);
     //保存
     post.save(function(err) {
       if(err) {
@@ -202,6 +205,84 @@ module.exports = function(app) {
     res.redirect('/upload');
   });
 
+
+  /**
+   * 获取存放
+   */
+  app.get('/archive',function(req,res) {
+    Post.getArchive(function(err,posts) {
+      if(err) {
+        req.flash('error',err);
+        return res.redirect('/');
+      }
+
+      res.render('archive',{
+        title: '存档',
+        posts: posts,
+        user: req.session.user,
+        success: req.flash('success').toString(),
+        error: req.flash('error').toString()
+      });
+    });
+  });
+
+
+  /**
+   * 获取标签
+   */
+  app.get('/tags',function(req,res) {
+    Post.getTags(function (err,posts) {
+      if(err) {
+        req.flash('error',err);
+        return res.redirect('/');
+      }
+
+      res.render('tags',{
+        title:'标签',
+        posts: posts,
+        user: req.session.user,
+        success: req.flash('success').toString(),
+        error: req.flash('error').toString()
+      });
+
+    });
+  });
+
+
+  app.get('/tags/:tag', function (req, res) {
+    Post.getTag(req.params.tag, function (err, posts) {
+      if (err) {
+        req.flash('error',err);
+        return res.redirect('/');
+      }
+      res.render('tag', {
+        title: 'TAG:' + req.params.tag,
+        posts: posts,
+        user: req.session.user,
+        success: req.flash('success').toString(),
+        error: req.flash('error').toString()
+      });
+    });
+  });
+
+  /**
+   * 搜索
+   */
+  app.get('/search', function (req, res) {
+    Post.search(req.query.keyword, function (err, posts) {
+      if (err) {
+        req.flash('error', err);
+        return res.redirect('/');
+      }
+      res.render('search', {
+        title: "SEARCH:" + req.query.keyword,
+        posts: posts,
+        user: req.session.user,
+        success: req.flash('success').toString(),
+        error: req.flash('error').toString()
+      });
+    });
+  });
 
 
   app.get('/u/:name',function(req, res) {
